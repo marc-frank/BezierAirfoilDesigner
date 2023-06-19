c5888eb0e82806b912683b30dd9a258db0db3a60
Marc Frank <marc.silas.frank@gmail.com>
2023-06-19 18:13:29 +0200

    update

--- BezierAirfoilDesigner/Form1.Designer.cs
+++ BezierAirfoilDesigner/Form1.Designer.cs
@@ -28,12 +28,131 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            components = new System.ComponentModel.Container();
+            formsPlot1 = new ScottPlot.FormsPlot();
+            dataGridView1 = new DataGridView();
+            topBindingSource = new BindingSource(components);
+            dataGridView2 = new DataGridView();
+            bottomBindingSource = new BindingSource(components);
+            label1 = new Label();
+            label2 = new Label();
+            button1 = new Button();
+            richTextBox1 = new RichTextBox();
+            label3 = new Label();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)topBindingSource).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)bottomBindingSource).BeginInit();
+            SuspendLayout();
+            // 
+            // formsPlot1
+            // 
+            formsPlot1.Location = new Point(0, 0);
+            formsPlot1.Margin = new Padding(5, 4, 5, 4);
+            formsPlot1.Name = "formsPlot1";
+            formsPlot1.Size = new Size(1260, 857);
+            formsPlot1.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(1268, 31);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 47;
+            dataGridView1.RowTemplate.Height = 28;
+            dataGridView1.Size = new Size(279, 385);
+            dataGridView1.TabIndex = 1;
+            // 
+            // dataGridView2
+            // 
+            dataGridView2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView2.Location = new Point(1268, 441);
+            dataGridView2.Name = "dataGridView2";
+            dataGridView2.RowHeadersWidth = 47;
+            dataGridView2.RowTemplate.Height = 28;
+            dataGridView2.Size = new Size(279, 385);
+            dataGridView2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(1268, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(31, 19);
+            label1.TabIndex = 3;
+            label1.Text = "Top";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(1268, 419);
+            label2.Name = "label2";
+            label2.Size = new Size(55, 19);
+            label2.TabIndex = 4;
+            label2.Text = "Bottom";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(1674, 832);
+            button1.Name = "button1";
+            button1.Size = new Size(86, 26);
+            button1.TabIndex = 5;
+            button1.Text = "set";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(1553, 31);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(207, 795);
+            richTextBox1.TabIndex = 6;
+            richTextBox1.Text = "";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(1553, 9);
+            label3.Name = "label3";
+            label3.Size = new Size(53, 19);
+            label3.TabIndex = 7;
+            label3.Text = ".dat file";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 19F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1772, 870);
+            Controls.Add(label3);
+            Controls.Add(richTextBox1);
+            Controls.Add(button1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(dataGridView2);
+            Controls.Add(dataGridView1);
+            Controls.Add(formsPlot1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)topBindingSource).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)bottomBindingSource).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private ScottPlot.FormsPlot formsPlot1;
+        private DataGridView dataGridView1;
+        private BindingSource topBindingSource;
+        private DataGridView dataGridView2;
+        private BindingSource bottomBindingSource;
+        private Label label1;
+        private Label label2;
+        private Button button1;
+        private RichTextBox richTextBox1;
+        private Label label3;
     }
 }
--- BezierAirfoilDesigner/Form1.cs
+++ BezierAirfoilDesigner/Form1.cs
@@ -1,3 +1,5 @@
+using System.Windows.Forms;
+
 namespace BezierAirfoilDesigner
 {
     public partial class Form1 : Form
@@ -5,6 +7,199 @@ namespace BezierAirfoilDesigner
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            calculations();
+        }
+
+
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            //formsPlot1.SetBounds(12, 10, Form1.ActiveForm.Width - 527, Form1.ActiveForm.Height - 67);
+            //richTextBox1.SetBounds(Form1.ActiveForm.Width - 234, 31, 204, Form1.ActiveForm.Height - 88);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {            
+            dataGridView1.AllowUserToResizeColumns = false;
+            dataGridView1.AllowUserToResizeRows = false;
+            dataGridView2.AllowUserToResizeColumns = false;
+            dataGridView2.AllowUserToResizeRows = false;
+
+            dataGridView1.Columns.Add("xVal", "X");
+            dataGridView1.Columns.Add("yVal", "Y");
+            dataGridView1.Rows.Add(0, 0);
+            dataGridView1.Rows.Add(0, 0.15);
+            dataGridView1.Rows.Add(0.5, 0.15);
+            dataGridView1.Rows.Add(1.0, 0.0);
+
+            dataGridView2.Columns.Add("xVal", "X");
+            dataGridView2.Columns.Add("yVal", "Y");
+            dataGridView2.Rows.Add(0, 0);
+            dataGridView2.Rows.Add(0, -0.1);
+            dataGridView2.Rows.Add(0.5, -0.1);
+            dataGridView2.Rows.Add(1.0, 0.0);
+
+            calculations();
+        }
+
+        void calculations()
+        {
+
+            List<PointF> controlPointsTop = new List<PointF>();
+            for(int i = 0; i < dataGridView1.Rows.Count - 1; i++)
+            {
+                // Retrieve the values from the DataGridView
+                float xt = float.Parse(dataGridView1.Rows[i].Cells[0].Value.ToString());
+                float yt = float.Parse(dataGridView1.Rows[i].Cells[1].Value.ToString());
+                // Create a PointF object
+                PointF pointT = new PointF(xt, yt);
+                controlPointsTop.Add(pointT);
+            }
+
+
+
+            List<PointF> controlPointsBottom = new List<PointF>();
+            for(int i = 0; i < dataGridView2.Rows.Count - 1; i++)
+            {
+                // Retrieve the values from the DataGridView
+                float xb = float.Parse(dataGridView2.Rows[i].Cells[0].Value.ToString());
+                float yb = float.Parse(dataGridView2.Rows[i].Cells[1].Value.ToString());
+                // Create a PointF object
+                PointF pointB = new PointF(xb, yb);
+                controlPointsBottom.Add(pointB);
+            }
+
+
+            //if (float.Parse(textBox9.Text) < 3) { textBox9.Text = "3"; }
+            //if (float.Parse(textBox18.Text) < 3) { textBox18.Text = "3"; }
+
+            //PointF P0_top = new PointF(float.Parse(textBox1.Text, System.Globalization.CultureInfo.InvariantCulture), float.Parse(textBox2.Text, System.Globalization.CultureInfo.InvariantCulture));
+            //PointF P1_top = new PointF(float.Parse(textBox3.Text, System.Globalization.CultureInfo.InvariantCulture), float.Parse(textBox4.Text, System.Globalization.CultureInfo.InvariantCulture));
+            //PointF P2_top = new PointF(float.Parse(textBox5.Text, System.Globalization.CultureInfo.InvariantCulture), float.Parse(textBox6.Text, System.Globalization.CultureInfo.InvariantCulture));
+            //PointF P3_top = new PointF(float.Parse(textBox7.Text, System.Globalization.CultureInfo.InvariantCulture), float.Parse(textBox8.Text, System.Globalization.CultureInfo.InvariantCulture));
+            //int numPointsTop = int.Parse(textBox9.Text);
+
+            //PointF P0_bottom = new PointF(float.Parse(textBox10.Text, System.Globalization.CultureInfo.InvariantCulture), float.Parse(textBox11.Text, System.Globalization.CultureInfo.InvariantCulture));
+            //PointF P1_bottom = new PointF(float.Parse(textBox12.Text, System.Globalization.CultureInfo.InvariantCulture), float.Parse(textBox13.Text, System.Globalization.CultureInfo.InvariantCulture));
+            //PointF P2_bottom = new PointF(float.Parse(textBox14.Text, System.Globalization.CultureInfo.InvariantCulture), float.Parse(textBox15.Text, System.Globalization.CultureInfo.InvariantCulture));
+            //PointF P3_bottom = new PointF(float.Parse(textBox16.Text, System.Globalization.CultureInfo.InvariantCulture), float.Parse(textBox17.Text, System.Globalization.CultureInfo.InvariantCulture));
+            //int numPointsBottom = int.Parse(textBox18.Text);
+
+            //List<PointF> controlPointsTop = new List<PointF> { P0_top, P1_top, P2_top, P3_top };
+            //List<PointF> controlPointsBottom = new List<PointF> { P0_bottom, P1_bottom, P2_bottom, P3_bottom };
+
+            //--------------------------------------------------------------------------------------------------
+
+            int numPointsTop = 100;
+            int numPointsBottom = 100;
+
+
+            List<PointF> pointsTop = DeCasteljau.BezierCurve(controlPointsTop, numPointsTop);
+            List<PointF> pointsBottom = DeCasteljau.BezierCurve(controlPointsBottom, numPointsBottom);
+
+            //--------------------------------------------------------------------------------------------------
+
+            for (int i = 0; i <= controlPointsTop.Count - 1; i++)
+            {
+                richTextBox1.AppendText(controlPointsTop[i].X.ToString() + " " + controlPointsTop[i].Y.ToString() + "\n");
+            }
+
+            richTextBox1.AppendText("\n");
+
+            for (int i = 0; i <= controlPointsBottom.Count - 1; i++)
+            {
+                richTextBox1.AppendText(controlPointsBottom[i].X.ToString() + " " + controlPointsBottom[i].Y.ToString() + "\n");
+            }
+
+            richTextBox1.AppendText("\n");
+
+            //--------------------------------------------------------------------------------------------------
+
+            richTextBox1.Text = "" + "Airfoil Name\n";
+
+            for (int i = pointsTop.Count - 1; i >= 0; i--)
+            {
+                richTextBox1.AppendText($"{pointsTop[i].X:0.00000000} {pointsTop[i].Y:0.00000000}" + "\n");
+            }
+
+            for (int i = 1; i <= pointsBottom.Count - 1; i++)
+            {
+                richTextBox1.AppendText($"{pointsBottom[i].X:0.00000000} {pointsBottom[i].Y:0.00000000}" + "\n");
+            }
+
+            richTextBox1.Text = richTextBox1.Text.Replace(',', '.');
+
+            //--------------------------------------------------------------------------------------------------
+
+            formsPlot1.Plot.Clear();
+
+            var topControl = formsPlot1.Plot.AddScatterList(color: Color.Black, lineStyle: ScottPlot.LineStyle.Dash);
+            var bottomControl = formsPlot1.Plot.AddScatterList(color: Color.Black, lineStyle: ScottPlot.LineStyle.Dash);
+
+            for (int i = 0; i < controlPointsTop.Count; i++)
+            {
+                topControl.Add(controlPointsTop[i].X, controlPointsTop[i].Y);
+            }
+            for (int i = 0; i < controlPointsBottom.Count; i++)
+            {
+                bottomControl.Add(controlPointsBottom[i].X, controlPointsBottom[i].Y);
+            }
+
+            var top = formsPlot1.Plot.AddScatterList(color: Color.Red, lineStyle: ScottPlot.LineStyle.Solid);
+            var bottom = formsPlot1.Plot.AddScatterList(color: Color.Red, lineStyle: ScottPlot.LineStyle.Solid);
+
+            for (int i = 0; i < pointsTop.Count; i++)
+            {
+                top.Add(pointsTop[i].X, pointsTop[i].Y);
+            }
+            for (int i = 0; i < pointsBottom.Count; i++)
+            {
+                bottom.Add(pointsBottom[i].X, pointsBottom[i].Y);
+            }
+
+            formsPlot1.Plot.AxisScaleLock(enable: true, scaleMode: ScottPlot.EqualScaleMode.PreserveX);
+            formsPlot1.Refresh();
+        }
+
+        public class DeCasteljau
+        {
+            // Calculate a point on a Bezier curve using the de Casteljau algorithm
+            private static PointF DeCasteljauPoint(List<PointF> controlPoints, double t)
+            {
+                List<PointF> points = new List<PointF>(controlPoints);
+                int n = points.Count;
+
+                for (int r = 1; r < n; r++)
+                {
+                    for (int i = 0; i < n - r; i++)
+                    {
+                        float x = (1 - (float)t) * points[i].X + (float)t * points[i + 1].X;
+                        float y = (1 - (float)t) * points[i].Y + (float)t * points[i + 1].Y;
+                        points[i] = new PointF(x, y);
+                    }
+                }
+
+                return points[0];
+            }
+
+            // Calculate points on a Bezier curve using the de Casteljau algorithm
+            public static List<PointF> BezierCurve(List<PointF> controlPoints, int nPoints)
+            {
+                List<PointF> points = new List<PointF>();
+                double step = 1.0 / (nPoints - 1);
+
+                for (int i = 0; i < nPoints; i++)
+                {
+                    double t = i * step;
+                    points.Add(DeCasteljauPoint(controlPoints, t));
+                }
+
+                return points;
+            }
         }
     }
 }
