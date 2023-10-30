1b7a0bc561c64c336371dbfa097c85a615edc5b7
Marc Frank <marc.silas.frank@gmail.com>
2023-10-30 19:34:10 +0100

    moved the PointD class to its own file

---
+++ BezierAirfoilDesigner/PointD.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BezierAirfoilDesigner
+{
+    public class PointD : IEquatable<PointD>
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public PointD(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public PointD()
+        {
+        }
+
+        public bool Equals(PointD other)
+        {
+            if (other == null)
+                return false;
+
+            return X == other.X && Y == other.Y;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+                return false;
+
+            return Equals((PointD)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(X, Y);
+        }
+    }
+}
